c0d450cb995937c5803fddf8cc5434adae4216f7
namseunghyeon <namsh1125@naver.com>
2024-11-12 07:27:28 +0900

    Fix: Subnet 수정 (prod 환경의 public, private Subnet 개수를 각각 1개씩 수정)

--- vpc/main.tf
+++ vpc/main.tf
@@ -12,22 +12,22 @@ module "vpc" {
   cidr = var.vpc_cidr_block
 
   # Availability Zone
-  azs = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
+  azs = ["ap-northeast-2a", "ap-northeast-2b"]
 
   # Public Subnet
-  public_subnets = [var.prod_public_subnet1_cidr_block, var.prod_public_subnet2_cidr_block, var.stage_public_subnet_cidr_block]
-  public_subnet_names = ["${module.common.project}-prod-public-subnet1", "${module.common.project}-prod-public-subnet2", "${module.common.project}-stage-public-subnet"]
+  public_subnets = [var.prod_public_subnet_cidr_block, var.stage_public_subnet_cidr_block]
+  public_subnet_names = ["${module.common.project}-prod-public-subnet", "${module.common.project}-stage-public-subnet"]
 
   # Private Subnet
-  private_subnets = [var.prod_private_subnet1_cidr_block, var.prod_private_subnet2_cidr_block]
-  private_subnet_names = ["${module.common.project}-prod-private-subnet1", "${module.common.project}-prod-private-subnet2"]
+  private_subnets = [var.prod_private_subnet_cidr_block]
+  private_subnet_names = ["${module.common.project}-prod-private-subnet"]
 
   # Internet Gateway
   igw_tags = {
     Name = "${module.common.project}-igw"
   }
 
-  # NAT Gateway - 한 개의 NAT Gateway를 사용
+  # NAT Gateway - 한 개의 NAT Gateway를 사용, 첫 번째 public subnet에 생성됨
   enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
@@ -46,9 +46,9 @@ module "vpc" {
 
 # S3 Endpoint
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = module.vpc.vpc_id
+  vpc_id          = module.vpc.vpc_id
   route_table_ids = module.vpc.private_route_table_ids
-  service_name = "com.amazonaws.ap-northeast-2.s3"
+  service_name    = "com.amazonaws.ap-northeast-2.s3"
   tags = {
     Name = "${module.common.project}-s3-endpoint"
   }
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -4,32 +4,20 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "prod_public_subnet1_cidr_block" {
+variable "prod_public_subnet_cidr_block" {
   description = "(Prod) Public Subnet CIDR 블럭 (ap-northeast-2a)"
   type        = string
   default     = "10.0.0.0/20"
 }
 
-variable "prod_public_subnet2_cidr_block" {
-  description = "(Prod) Public Subnet CIDR 블럭 (ap-northeast-2b)"
-  type        = string
-  default     = "10.0.16.0/20"
-}
-
-variable "prod_private_subnet1_cidr_block" {
+variable "prod_private_subnet_cidr_block" {
   description = "(Prod) Private Subnet CIDR 블럭 (ap-northeast-2a)"
   type        = string
   default     = "10.0.128.0/20"
 }
 
-variable "prod_private_subnet2_cidr_block" {
-  description = "(Prod) Private Subnet CIDR 블럭 (ap-northeast-2b)"
-  type        = string
-  default     = "10.0.144.0/20"
-}
-
 variable "stage_public_subnet_cidr_block" {
-  description = "(Stage) Public Subnet CIDR 블럭 (ap-northeast-2c)"
+  description = "(Stage) Public Subnet CIDR 블럭 (ap-northeast-2b)"
   type        = string
   default     = "10.0.240.0/20"
 }
